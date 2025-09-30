6045be60d6b30f68f1eb7533837a38fb7d9e894f
MarcLester021 <mlsulit@gbox.adnu.edu.ph>
2025-10-01 00:57:00 +0800

    latest commit for studentControllers

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,6 +1,6 @@
 import studentService from "../services/studentService.js";
 
-export const getStudent = async (req, res) => {
+export const getAllStudents = async (req, res) => {
     try {
         const students = await studentService.getAllStudents();
         res.json(students);
@@ -17,3 +17,39 @@ export const createStudent = async (req, res) => {
         res.status(400).json(({ message: error.message }));
     }
 }
+
+export const getStudentById = async (req, res) => {
+    try {
+        const student = await studentService.getStudentById(req.params.id);
+        if (!student) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+        res.json(student);
+    } catch (error) {
+        res.status(500).json(({ message: error.message }));
+    }   
+}
+export const updateStudent = async (req, res) => {
+    try {
+        const student = await studentService.updateStudent(req.params.id, req.body);
+        if (!student) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+        res.json(student);
+    } catch (error) {
+        res.status(400).json(({ message: error.message }));
+    }   
+}
+
+export const deleteStudent = async (req, res) => {
+    try {
+        const student = await studentService.deleteStudent(req.params.id);
+        if (!student) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+        res.json({ message: "Student deleted successfully" });
+    }    
+    catch (error) {
+        res.status(500).json(({ message: error.message }));
+    }
+} 
