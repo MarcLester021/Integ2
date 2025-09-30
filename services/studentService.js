f1aba2b2b89ff6198a854e6fd0dbe7ae5bb67df5
MarcLester021 <mlsulit@gbox.adnu.edu.ph>
2025-10-01 01:06:35 +0800

    latest fix

--- services/studentService.js
+++ services/studentService.js
@@ -7,6 +7,17 @@ const getAllStudents = async () => {
 const createStudent = async (data) => {
     const student = new Student(data);
     return await student.save();
+}
+
+const getStudentById = async (id) => {
+    return await Student.findOne({ id });
+}
+const updateStudent = async (id, data) => {
+    return await Student.findOneAndUpdate({ id }, data, { new: true, runValidators: true });
+}
+
+const deleteStudent = async (id) => {
+    return await Student.findOneAndDelete({ id });
 };
 
-export default { getAllStudents, createStudent };
+export default { getAllStudents, createStudent, getStudentById, updateStudent, deleteStudent };
