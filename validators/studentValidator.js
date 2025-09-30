197748b17ce970bd8494d52055ccd6e07b13b064
MarcLester021 <mlsulit@gbox.adnu.edu.ph>
2025-10-01 00:17:36 +0800

    minor fix

--- validators/studentValidator.js
+++ validators/studentValidator.js
@@ -1,7 +1,7 @@
 export const validateStudent = (req, res, next) => {
-    const { name, email } = req.body;
+    const { name, id } = req.body;
     
-    if (!name || !email) {
+    if (!name || !id) {
         return res.status(400).json({ message: "Name and id are required."});
     }
     next();
