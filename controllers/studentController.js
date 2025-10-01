2341697e4feebcac0faf5dfc83aac62f21012eb7
MarcLester021 <mlsulit@gbox.adnu.edu.ph>
2025-10-01 22:52:02 +0800

    final commit

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,56 +1,25 @@
 import studentService from "../services/studentService.js";
 import Student from "../models/Student.js";
 
-export const getAllStudents = async () => {
-    try {
-        const students = await studentService.getAllStudents();
-        res.json(students);
-    } catch (error) {
-        res.status(500).json(({ message: error.message }));
-    }
+const getAllStudents = async () => {
+  return await Student.find(); 
 }
 
-export const addStudent = async (data) => {
-    try {
-        const student = await studentService.addStudent(data);
-        res.status(201).json(student);
-    } catch (error) {
-        res.status(400).json(({ message: error.message }));
-    }
+const addStudent = async (data) => {
+  const newStudent = new Student(data);
+  return await newStudent.save();
 }
 
-export const getStudentById = async (id) => {
-    try {
-        const student = await studentService.getStudentById({ id });
-        if (!student) {
-            return res.status(404).json({ message: "Student not found" });
-        }
-        res.json(student);
-    } catch (error) {
-        res.status(500).json(({ message: error.message }));
-    }   
-}
-export const updateStudent = async (id, data) => {
-    try {
-        const student = await studentService.updateStudent({ id }, data, { new: true });
-        if (!student) {
-            return res.status(404).json({ message: "Student not found" });
-        }
-        res.json(student);
-    } catch (error) {
-        res.status(400).json(({ message: error.message }));
-    }   
+const getStudentById = async (id) => {
+  return await Student.findOne({ id });
 }
 
-export const deleteStudent = async (id) => {
-    try {
-        const student = await studentService.deleteStudent({ id });
-        if (!student) {
-            return res.status(404).json({ message: "Student not found" });
-        }
-        res.json({ message: "Student deleted successfully" });
-    }    
-    catch (error) {
-        res.status(500).json(({ message: error.message }));
-    }
-} 
+const updateStudent = async (id, data) => {
+  return await Student.findOneAndUpdate({ id }, data, { new: true });
+}
+
+const deleteStudent = async (id) => {
+  return await Student.findOneAndDelete({ id });
+}
+
+export { getAllStudents, addStudent, getStudentById, updateStudent, deleteStudent,};
